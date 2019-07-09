db691f21edf77df308f62163000f97584b3cc5ec
Samer Mohamed <Samirsuraj@live.com>
2019-07-10 00:57:03 +0100

    multi not tested

--- concept.py
+++ concept.py
@@ -3,17 +3,26 @@ import matplotlib.pyplot as plt
 import random
 import queue
 
+concepts=set('target', 'pro', 'anti')
+
 def randseed(g, size):
     return random.choices(g.nodes,k=size)
 
 class Concept():
     def __init__(self,g,name,seedsize=10,func=randseed,factor=0.9):
         self.factor=factor
+        self.name=name
+        #set up multicpncept influence factors for pr ant case
+        self.r=zip(concepts,[1,1,1])#influence for reciever and sender is the same
+        if name='target':
+            self.r['pro']=1.1
+            self.r['anti']=0.9
+        #set up seed sets
         self.seed=func(g,seedsize)
         self.newNodes=seed
-        self.name=name
         for node in seed:
-            g.nodes[node][name]=True
+            g.nodes[node]['concept']=set(name)
+
 
     def update(self):
         #loops over all the recently activated nodes, rather than interwining concept activation
@@ -23,8 +32,29 @@ class Concept():
             neigh=g.neighbors(node)
             for n in neigh:
                 p=random.random()
-                if p < self.factor/len(neigh): #NONWEIGHTED GRAPH
-                    if g.nodes[n][self.name]==False:
-                        g.nodes[n][self.name]=True
+                CI=self.context(node,n)#NONWEIGHTED GRAPH
+                if p < CI:
+                    try: #concepts activate once
+                        if not self.name in g.nodes[n]['concept']:
+                            g.nodes[n]['concept'].add(self.name)
+                            newN.append(n)
+                    except KeyError:
+                        g.nodes[n]['concept']=set(self.name)
                         newN.append(n)
         self.newNodes=newN
+
+    def context(send, rec):
+        ci=self.factor
+        try:
+            for c in send['concept']:
+                ci=ci*self.r[c]
+        except KeyError:
+            pass
+        try:
+            for c in rec['concept']:
+                ci=ci*self.r[c]
+        except KeyError:
+            pass
+
+        ci=min(1,ci)
+        return ci
